ef7acefb67df96adca599778617987e9859bbe1a
Jonty Morris <jonty.morris@yahoo.co.nz>
2019-10-06 02:18:27 +1300

    Changed message

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("App listening at http://%s:%s", host, port)
 })
