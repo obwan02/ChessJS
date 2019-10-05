9092059b3a0706c6cbaa1f853d6d7256095aee8f
Jonty Morris <jonty.morris@yahoo.co.nz>
2019-10-06 02:19:17 +1300

    Changed to tabs

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ var app = express()
 app.use(express.static('./src'))
 
 var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("App listening at http://%s:%s", host, port)
+	var host = server.address().address
+	var port = server.address().port
+	console.log("App listening at http://%s:%s", host, port)
 })
